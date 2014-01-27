a5532f4d97ce784aa0ba72fa634e7795291e5418
Andrey Repin <aarepin@gmail.com>
2014-01-27 19:34:51 +0200

    minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Catalog::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'companies#index'
+   root 'text_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
